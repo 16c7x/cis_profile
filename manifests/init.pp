acaaa7fb7ad847ad9061d9342089b7cebf23ce69
andrew <andrew.jones@puppet.com>
2020-09-22 18:06:10 +0100

    removing debug messages

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,6 @@ class cis_profile (
   Array[String]                           $server_level_2          = [],
   Array[String]                           $exclude_rules           = [],
 ){
-    notify { $exclude_rules: }
     class {'::secure_linux_cis':
     time_servers     => ['time1.google.com', 'time1.google.com'],
     profile_type     => 'server',
