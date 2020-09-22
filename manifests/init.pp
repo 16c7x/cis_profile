80528bbcb0185f091d0372653af896e2abc4818b
Andrew Jones <pic16c7x@gmail.com>
2020-09-22 13:14:48 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,13 @@
 #
-class cis_profile {
+class cis_profile (
+Array[String]                           $workstation_level_1     = [],
+Enum['firewall', 'firewalld']           $firewall_package,
+){
     class {'::secure_linux_cis':
     time_servers  => ['time1.google.com', 'time1.google.com'],
     profile_type  => 'server',
     allow_users   => ['centos'],
-    firewall_package => 'firewalld'
-    server_level_1 => 
+    firewall_package => $firewall_package,
+    server_level_1 => $workstation_level_1 ,
   }
 }
