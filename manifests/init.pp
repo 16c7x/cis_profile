dea9875bfce85fc13ccb5501a754c947bfa26a80
andrew <andrew.jones@puppet.com>
2020-09-22 16:43:23 +0100

    adding in some more heira

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,18 @@
 #
 class cis_profile (
-Array[String]                           $workstation_level_1     = [],
-Array[String]                           $exclude_rules           = [],
-Enum['firewall', 'firewalld']           $firewall_package,
+  Enum['firewall', 'firewalld']           $firewall_package,
+  Array[String]                           $server_level_1          = [],
+  Array[String]                           $server_level_2          = [],
+  Array[String]                           $exclude_rules           = [],
 ){
     notify { $firewall_package: }
     class {'::secure_linux_cis':
-    time_servers  => ['time1.google.com', 'time1.google.com'],
-    profile_type  => 'server',
-    allow_users   => ['centos'],
+    time_servers     => ['time1.google.com', 'time1.google.com'],
+    profile_type     => 'server',
+    allow_users      => ['centos'],
     firewall_package => $firewall_package,
-    server_level_1 => $workstation_level_1 ,
-    exclude_rules => $exclude_rules,
+    server_level_1   => $server_level_1 ,
+    server_level_2   => $server_level_2 ,
+    exclude_rules    => $exclude_rules,
   }
 }
