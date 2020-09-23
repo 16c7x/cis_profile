c65a4f8dbc25f5c639fe212b5ad344f5b220f877
andrew <andrew.jones@puppet.com>
2020-09-23 16:10:09 +0100

    removing .vscode

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,13 +7,13 @@ class cis_profile (
 ){
 
     notify { $exclude_rules: }
-    class {'::secure_linux_cis':
-    time_servers     => ['time1.google.com', 'time1.google.com'],
-    profile_type     => 'server',
-    allow_users      => ['centos'],
-    firewall_package => $firewall_package,
-    server_level_1   => $server_level_1 ,
-    server_level_2   => $server_level_2 ,
-    exclude_rules    => $exclude_rules,
-  }
+    #class {'::secure_linux_cis':
+    #time_servers     => ['time1.google.com', 'time1.google.com'],
+    #profile_type     => 'server',
+    #allow_users      => ['centos'],
+    #firewall_package => $firewall_package,
+    #server_level_1   => $server_level_1 ,
+    #server_level_2   => $server_level_2 ,
+    #exclude_rules    => $exclude_rules,
+  #}
 }
