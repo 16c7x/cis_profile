d2c4a1394c500396d154213112d154059fa5f47f
andrew <andrew.jones@puppet.com>
2020-09-23 17:00:45 +0100

    hiera merge strategy

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,15 +5,13 @@ class cis_profile (
   Array[String]                           $server_level_2          = [],
   Array[String]                           $exclude_rules           = [],
 ){
-
-    notify { $exclude_rules: }
-    #class {'::secure_linux_cis':
-    #time_servers     => ['time1.google.com', 'time1.google.com'],
-    #profile_type     => 'server',
-    #allow_users      => ['centos'],
-    #firewall_package => $firewall_package,
-    #server_level_1   => $server_level_1 ,
-    #server_level_2   => $server_level_2 ,
-    #exclude_rules    => $exclude_rules,
-  #}
+  class {'::secure_linux_cis':
+    time_servers     => ['time1.google.com', 'time1.google.com'],
+    profile_type     => 'server',
+    allow_users      => ['centos'],
+    firewall_package => $firewall_package,
+    server_level_1   => $server_level_1 ,
+    server_level_2   => $server_level_2 ,
+    exclude_rules    => $exclude_rules,
+  }
 }
