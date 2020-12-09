a44c526191a78aab978ecec1a403587f9791d202
Andrew Jones <pic16c7x@gmail.com>
2020-12-09 17:35:44 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,14 @@
 # This profile implements https://forge.puppet.com/fervid/secure_linux_cis
 # Check the secure_linux_cis module if you want to expose more params.
-# @param firewall Which Firewall provider to use
-# @param enforcement_level Which level to enforce
-# @param server_level_1 List of CIS server rules level 1
-# @param server_level_2 List of CIS server rules level 2
 # @param exclude_rules Which rules to include
 
 class cis_profile (
-  Enum['firewall', 'firewalld']           $firewall_package,
-  Enum['1', '2']                          $enforcement_level       = '1',
-  Array[String]                           $server_level_1          = [],
   Array[String]                           $exclude_rules           = [],
 ){
   class {'::secure_linux_cis':
     time_servers     => ['time1.google.com', 'time1.google.com'],
-    profile_type     => 'server',
     allow_users      => ['centos'],
-    firewall_package => $firewall_package,
-    server_level_1   => $server_level_1 ,
+    firewall_package => 'firewalld',
     exclude_rules    => $exclude_rules,
   }
   notify { $exclude_rules: }
